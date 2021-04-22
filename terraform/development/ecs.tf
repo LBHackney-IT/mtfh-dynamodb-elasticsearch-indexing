ef9a3182ff67ce91c3583bcc7a73ee7a80ba6ce4
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-04-22 14:21:42 +0100

    Added ECS task.

--- terraform/development/ecs.tf
+++ terraform/development/ecs.tf
@@ -1,4 +1,37 @@
 resource "aws_ecs_cluster" "cluster" {
   name               = var.ecs_cluster_name
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  tags = {
+    Name              = "${var.repo_name}-${var.environment_name}"
+    Environment       = var.environment_name
+    terraform-managed = true
+    project_name      = var.project_name
+  }
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.repo_name}-${var.environment_name}-task"
+  execution_role_arn       = var.execution_role
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  container_definitions    = jsonencode(
+  [
+    {
+      name      = var.repo_name
+      image     = "${var.ecr_host}/${var.ecr_repo_name}:${var.ecr_image_sha1}"
+      cpu       = 256
+      memory    = 512
+      essential = true
+    }
+  ])
+
+  tags = {
+    Name              = "${var.repo_name}-${var.environment_name}"
+    Environment       = var.environment_name
+    terraform-managed = true
+    project_name      = var.project_name
+  }
 }
--- terraform/development/variables.tf
+++ terraform/development/variables.tf
@@ -8,7 +8,24 @@ variable "project_name" {
     default = "Housing-Development"
 }
 
+variable "repo_name" {
+    type = string
+    default = "mfth-dynamodb-elasticsearch-indexing"
+}
+
 variable "ecs_cluster_name" {
     type = string
     default = "mfth-dynamodb-elasticsearch-indexing"
+}
+
+variable "ecr_host" {
+    type = string
+}
+
+variable "ecr_repo_name" {
+    type = string
+}
+
+variable "ecr_image_sha1" {
+    type = string
 }
