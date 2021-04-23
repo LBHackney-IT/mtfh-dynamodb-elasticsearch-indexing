45864c495b663fa7508ac53fe3ed4bfb9e0ba06f
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-04-23 09:53:03 +0100

    Refactored Lambda function.

--- DynamoDBIndexing/Function.cs
+++ DynamoDBIndexing/Function.cs
@@ -1,9 +1,10 @@
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using Amazon.Lambda.Core;
+using Amazon.ECS;
 
 using DynamoDBIndexing.Domain;
-using DynamoDBIndexing.UseCase;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -25,8 +26,40 @@ namespace DynamoDBIndexing
             context.Logger.LogLine($"dynamoTable: {input.DynamoTable}");
             context.Logger.LogLine($"IndexName: {input.IndexName}");
 
-            SyncPersonData syncPersonData = new SyncPersonData();
-            await syncPersonData.ExecuteSyncPersonData(input.DynamoTable, input.IndexNodeHost, input.IndexName, context);
+            var AmazonECS = new AmazonECSClient();
+
+            Amazon.ECS.Model.ContainerOverride containerOverride = new Amazon.ECS.Model.ContainerOverride()
+                        {
+                            Environment = new List<Amazon.ECS.Model.KeyValuePair>()
+                            {
+                                new Amazon.ECS.Model.KeyValuePair() {Name = "DYNAMODB_TABLE", Value = input.DynamoTable},
+                                new Amazon.ECS.Model.KeyValuePair() {Name = "ELASTICSEARCH_HOST", Value = input.IndexNodeHost},
+                                new Amazon.ECS.Model.KeyValuePair() {Name = "ELASTICSEARCH_INDEX", Value = input.IndexName},
+                            }
+                        };
+            
+            var results = await AmazonECS.RunTaskAsync(new Amazon.ECS.Model.RunTaskRequest()
+                {
+                    Cluster = "mfth-dynamodb-elasticsearch-indexing",
+                    LaunchType = LaunchType.FARGATE,
+                    TaskDefinition = "arn:aws:ecs:eu-west-2:364864573329:task-definition/mfth-dynamodb-elasticsearch-indexing-development-task:2",
+                    Count = 1,
+                    NetworkConfiguration = new Amazon.ECS.Model.NetworkConfiguration()
+                    {
+                        AwsvpcConfiguration = new Amazon.ECS.Model.AwsVpcConfiguration()
+                        {
+                            Subnets = new List<string>() {"subnet-0140d06fb84fdb547", "subnet-05ce390ba88c42bfd"},
+                            AssignPublicIp = AssignPublicIp.DISABLED,
+                        },
+                    },
+                    Overrides = new Amazon.ECS.Model.TaskOverride()
+                    {
+                        ContainerOverrides = new List<Amazon.ECS.Model.ContainerOverride>()
+                        {
+                            containerOverride
+                        }
+                    }
+                });
 
             await Task.CompletedTask;
         }
