e7c5ddb2cdcceedcbaf8da1b40623b5ea992c8f9
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-04-23 09:54:29 +0100

    Code formatting.

--- DynamoDBIndexing/Function.cs
+++ DynamoDBIndexing/Function.cs
@@ -29,37 +29,37 @@ namespace DynamoDBIndexing
             var AmazonECS = new AmazonECSClient();
 
             Amazon.ECS.Model.ContainerOverride containerOverride = new Amazon.ECS.Model.ContainerOverride()
-                        {
-                            Environment = new List<Amazon.ECS.Model.KeyValuePair>()
+            {
+                Environment = new List<Amazon.ECS.Model.KeyValuePair>()
                             {
                                 new Amazon.ECS.Model.KeyValuePair() {Name = "DYNAMODB_TABLE", Value = input.DynamoTable},
                                 new Amazon.ECS.Model.KeyValuePair() {Name = "ELASTICSEARCH_HOST", Value = input.IndexNodeHost},
                                 new Amazon.ECS.Model.KeyValuePair() {Name = "ELASTICSEARCH_INDEX", Value = input.IndexName},
                             }
-                        };
-            
+            };
+
             var results = await AmazonECS.RunTaskAsync(new Amazon.ECS.Model.RunTaskRequest()
+            {
+                Cluster = "mfth-dynamodb-elasticsearch-indexing",
+                LaunchType = LaunchType.FARGATE,
+                TaskDefinition = "arn:aws:ecs:eu-west-2:364864573329:task-definition/mfth-dynamodb-elasticsearch-indexing-development-task:2",
+                Count = 1,
+                NetworkConfiguration = new Amazon.ECS.Model.NetworkConfiguration()
                 {
-                    Cluster = "mfth-dynamodb-elasticsearch-indexing",
-                    LaunchType = LaunchType.FARGATE,
-                    TaskDefinition = "arn:aws:ecs:eu-west-2:364864573329:task-definition/mfth-dynamodb-elasticsearch-indexing-development-task:2",
-                    Count = 1,
-                    NetworkConfiguration = new Amazon.ECS.Model.NetworkConfiguration()
+                    AwsvpcConfiguration = new Amazon.ECS.Model.AwsVpcConfiguration()
                     {
-                        AwsvpcConfiguration = new Amazon.ECS.Model.AwsVpcConfiguration()
-                        {
-                            Subnets = new List<string>() {"subnet-0140d06fb84fdb547", "subnet-05ce390ba88c42bfd"},
-                            AssignPublicIp = AssignPublicIp.DISABLED,
-                        },
+                        Subnets = new List<string>() { "subnet-0140d06fb84fdb547", "subnet-05ce390ba88c42bfd" },
+                        AssignPublicIp = AssignPublicIp.DISABLED,
                     },
-                    Overrides = new Amazon.ECS.Model.TaskOverride()
-                    {
-                        ContainerOverrides = new List<Amazon.ECS.Model.ContainerOverride>()
+                },
+                Overrides = new Amazon.ECS.Model.TaskOverride()
+                {
+                    ContainerOverrides = new List<Amazon.ECS.Model.ContainerOverride>()
                         {
                             containerOverride
                         }
-                    }
-                });
+                }
+            });
 
             await Task.CompletedTask;
         }
