aa47f16dc84ae9aa48e15658ea18a97f82ce7c53
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-06-05 20:00:42 +0100

    Handle missing keys from dynamoDB.

--- DynamoDBIndexingCore/Domain/Person.cs
+++ DynamoDBIndexingCore/Domain/Person.cs
@@ -6,18 +6,29 @@ namespace DynamoDBIndexingCore.Domain
     public class Person
     {
         public string Id { get; set; }
+        #nullable enable
         public string? Title { get; set; }
+        #nullable enable
         public string? PreferredTitle { get; set; }
+        #nullable enable
         public string? PreferredFirstname { get; set; }
+        #nullable enable
         public string? PreferredMiddleName { get; set; }
+        #nullable enable
         public string? PreferredSurname { get; set; }
         public string Firstname { get; set; }
+        #nullable enable
         public string? MiddleName { get; set; }
         public string Surname { get; set; }
+        #nullable enable
         public string? DateOfBirth { get; set; }
+        #nullable enable
         public string? NationalInsuranceNo { get; set; }
+        #nullable enable
         public IEnumerable<Identification>? Identifications { get; set; }
+        #nullable enable
         public IEnumerable<Tenure>? Tenures { get; set; }
+        #nullable enable
         public IEnumerable<String>? PersonTypes { get; set; }
     }
 }
