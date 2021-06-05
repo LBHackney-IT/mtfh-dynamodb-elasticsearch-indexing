dc6ac4a27f1de75e2c1d42d317a999c34ef05417
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-06-05 20:16:10 +0100

    Handle missing keys from dynamoDB.

--- DynamoDBIndexingCore/Domain/Person.cs
+++ DynamoDBIndexingCore/Domain/Person.cs
@@ -6,29 +6,18 @@ namespace DynamoDBIndexingCore.Domain
     public class Person
     {
         public string Id { get; set; }
-        #nullable enable
-        public string? Title { get; set; }
-        #nullable enable
-        public string? PreferredTitle { get; set; }
-        #nullable enable
-        public string? PreferredFirstname { get; set; }
-        #nullable enable
-        public string? PreferredMiddleName { get; set; }
-        #nullable enable
-        public string? PreferredSurname { get; set; }
+        public string Title { get; set; }
+        public string PreferredTitle { get; set; }
+        public string PreferredFirstname { get; set; }
+        public string PreferredMiddleName { get; set; }
+        public string PreferredSurname { get; set; }
         public string Firstname { get; set; }
-        #nullable enable
-        public string? MiddleName { get; set; }
+        public string MiddleName { get; set; }
         public string Surname { get; set; }
-        #nullable enable
-        public string? DateOfBirth { get; set; }
-        #nullable enable
-        public string? NationalInsuranceNo { get; set; }
-        #nullable enable
-        public IEnumerable<Identification>? Identifications { get; set; }
-        #nullable enable
-        public IEnumerable<Tenure>? Tenures { get; set; }
-        #nullable enable
-        public IEnumerable<String>? PersonTypes { get; set; }
+        public string DateOfBirth { get; set; }
+        public string NationalInsuranceNo { get; set; }
+        public IEnumerable<Identification> Identifications { get; set; }
+        public IEnumerable<Tenure> Tenures { get; set; }
+        public IEnumerable<String> PersonTypes { get; set; }
     }
 }
