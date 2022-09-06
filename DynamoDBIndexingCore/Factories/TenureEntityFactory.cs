edddeb87b6dbac77596c17e88ed5c42df97bd551
humulla <43747248+humulla@users.noreply.github.com>
2022-09-06 10:27:15 +0100

    Resolve build issue

--- DynamoDBIndexingCore/Factories/TenureEntityFactory.cs
+++ DynamoDBIndexingCore/Factories/TenureEntityFactory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 using Amazon.DynamoDBv2.DocumentModel;
 using DynamoDBIndexingCore.Domain;
+using Amazon.Lambda.Core;
 
 namespace DynamoDBIndexingCore.Factories
 {
